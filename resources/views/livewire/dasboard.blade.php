25b198198b28263bde6b1a736c47c8ee3a4f49db
Orlando Ivan Basulto Benitez <gouiterra@gmail.com>
2021-06-02 04:05:49 +0200

    Agrege unas animaciones no del todo funcionales para el dasboard

---
+++ resources/views/livewire/dasboard.blade.php
@@ -0,0 +1,24 @@
+<div>
+    <button type="button" class="bg-rose-600 ..." disabled>
+    <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
+        <!-- ... -->
+    </svg>
+    Processing
+    </button>
+    <span class="flex h-3 w-3">
+        <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-purple-400 opacity-75"></span>
+        <span class="relative inline-flex rounded-full h-3 w-3 bg-purple-500"></span>
+    </span>
+    <div class="border border-light-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
+        <div class="animate-pulse flex space-x-4">
+            <div class="rounded-full bg-light-blue-400 h-12 w-12"></div>
+            <div class="flex-1 space-y-4 py-1">
+            <div class="h-4 bg-light-blue-400 rounded w-3/4"></div>
+            <div class="space-y-2">
+                <div class="h-4 bg-light-blue-400 rounded"></div>
+                <div class="h-4 bg-light-blue-400 rounded w-5/6"></div>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
